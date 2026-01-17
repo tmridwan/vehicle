1657d397106a3acf4f56245f5c83af1346fc25a4
tmridwan <tmridwan03@gmail.com>
2026-01-18 01:16:20 +0600

    Update app and requirements

--- app.py
+++ app.py
@@ -1,64 +1,71 @@
-from fastai.vision.all import *
 import gradio as gr
-import torch
+from fastai.vision.all import *
+from huggingface_hub import hf_hub_download
+import pathlib
 
+# Fix Windows -> Linux paths for pickled fastai learner
+pathlib.WindowsPath = pathlib.PosixPath
 
 vehicle_labels = [
-    # Cars & Light Vehicles
     "Pickup truck", "SUV", "Van / Minivan", "Station wagon",
-    "Convertible", "Sports car", "Hatchback", "Coupe", "Sedan", 
+    "Convertible", "Sports car", "Hatchback", "Coupe", "Sedan",
     "Limousine", "Taxi", "Police car", "Ambulance", "Fire truck",
-
-    # Trucks & Utility
-    "Light truck", "Heavy truck", "Semi truck", "Tow truck", 
+    "Light truck", "Heavy truck", "Semi truck", "Tow truck",
     "Garbage truck", "Cement mixer truck", "Dump truck",
     "Refrigerated truck", "Flatbed truck", "Tanker truck",
-
-    # Mass Transport
-    "Bus", "Mini-bus", "School bus", "Coach bus", 
+    "Bus", "Mini-bus", "School bus", "Coach bus",
     "Tram", "Train", "Subway train",
-
-    # Two- & Three-Wheelers
     "Bicycle", "Motorcycle", "Scooter", "Moped", "Dirt bike",
     "Three-wheeler", "Tricycle",
-
-    # Agriculture & Construction
     "Tractor", "Combine harvester", "Bulldozer", "Excavator",
-    "Backhoe loader", "Skid steer", "Forklift", 
+    "Backhoe loader", "Skid steer", "Forklift",
     "Road roller", "Crane",
-
-    # Aircraft
     "Airplane", "Helicopter", "Glider", "Hot air balloon",
     "Drone", "Jet", "Cargo aircraft", "Seaplane",
-
-    # Watercraft
     "Boat", "Ship", "Sailboat", "Yacht", "Speedboat",
     "Fishing boat", "Cargo ship", "Cruise ship",
     "Submarine", "Kayak", "Jet ski",
-
-    # Non-Motorized / Animal-Drawn
-    "Carriage", "Rickshaw", "Handcart", 
+    "Carriage", "Rickshaw", "Handcart",
     "Wheelchair", "Skateboard", "Roller skates"
 ]
 
-from fastai.vision.all import load_learner
-import pathlib, platform
+learn = None
 
-# Fix Windows → Linux paths
-if platform.system() != 'Windows':
-    pathlib.WindowsPath = pathlib.PosixPath
+def get_model():
+    global learn
+    if learn is None:
+        # If your model is in the Space repo, keep this:
+        # model_path = "vehicle_model.pkl"
 
-model = load_learner("vehicle_model.pkl", cpu=True)
+        # If your model is in HF model repo, use this instead:
+        model_path = hf_hub_download(
+            repo_id="tmridwan03/vehicle-classification-model",
+            filename="model.pkl"
+        )
+
+        learn = load_learner(model_path)  # don't force cpu=True here
+        if hasattr(learn, "dls"):         # avoid dls crash if missing
+            learn.dls.cpu()
+    return learn
 
 def recognize_image(image):
-    pred, idx, probs = model.predict(image)
-    return dict(zip(vehicle_labels, map(float, probs)))
+    model = get_model()
+    img = PILImage.create(image)
+    pred, idx, probs = model.predict(img)
 
-image = gr.Image(shape=(192,192))
-label = gr.Label(num_top_classes=5)
-examples = ['b.jpg', 'c.jpg', 'h.jpg', 'e.png']
+    top_probs, top_idxs = probs.topk(5)
+    top_probs = top_probs.tolist()
+    top_idxs = top_idxs.tolist()
 
+    return {vehicle_labels[int(i)]: float(p) for p, i in zip(top_probs, top_idxs)}
 
+demo = gr.Interface(
+    fn=recognize_image,
+    inputs=gr.Image(type="pil", label="Upload image"),
+    outputs=gr.Label(num_top_classes=5),
+    examples=["b.jpg", "c.jpg", "h.jpg", "e.png"],  # only if these files exist in repo
+    title="Vehicle Classifier"
+)
 
-iface = gr.Interface(fn=recognize_image, inputs=image, outputs=label, examples=examples)
-iface.launch(inline=False)
+if __name__ == "__main__":
+    demo.queue().launch(server_name="0.0.0.0", server_port=7860)
